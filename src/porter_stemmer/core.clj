000cd2781f14d6da3862eaba9c5ba4da4672862d
Basavanagowda Kanur <basavanagowda@gmail.com>
2012-09-11 05:41:28 +0530

    Define a procedure to find the difference between actual output and expected output, as per website

--- src/porter_stemmer/core.clj
+++ src/porter_stemmer/core.clj
@@ -1,5 +1,17 @@
 (ns porter-stemmer.core
+  (:require [clojure.string :as string])
   (:use [porter-stemmer.steps :only [stem-it]]))
+
+(defn find-differential [words-file output-file]
+  (let [words (string/split (slurp words-file) #"\n")
+        outputs (string/split (slurp output-file) #"\n")
+        gowtputs (map stem-it words)]
+    (reduce (fn [res [word gow porter]]
+              (if (= gow porter)
+                res
+                (assoc res word [gow porter])))
+            {}
+            (map (fn [w g p] [w g p]) words gowtputs outputs))))
 
 (defn conflate [strings]
   (let [output (map stem-it strings)]
