7104c0b24d252b3850acab5d7c5eddcb7b114323
Basavanagowda Kanur <basavanagowda@gmail.com>
2013-04-20 13:17:04 +0530

    Project description update

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject porter-stemmer "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "porter-stemmer: Porter Stemming Algorithm"
   :dependencies [[org.clojure/clojure "1.3.0"]])
