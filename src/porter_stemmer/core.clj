567217d869268749c50af16a1c90d1b0a343214b
Basavanagowda Kanur <basavanagowda@gmail.com>
2013-04-23 00:54:19 +0530

    Remove unused testing code from porter-stemmer.core

--- src/porter_stemmer/core.clj
+++ src/porter_stemmer/core.clj
@@ -6,24 +6,6 @@
 
 (def options [["-h" "--help" "Display this message" :default false :flag true]])
 
-(defn find-differential [words-file output-file]
-  (let [words (string/split (slurp words-file) #"\n")
-        outputs (string/split (slurp output-file) #"\n")
-        gowtputs (map stem-it words)]
-    (reduce (fn [res [word gow porter]]
-              (if (= gow porter)
-                res
-                (assoc res word [gow porter])))
-            {}
-            (map (fn [w g p] [w g p]) words gowtputs outputs))))
-
-(defn conflate [strings]
-  (let [output (map stem-it strings)]
-    (reduce (fn [res [word stem]]
-              (assoc res stem (conj (get res stem []) word)))
-            {}
-            (zipmap strings output))))
-
 (defn -main [& args]
   (let [[options-map words banner] (apply (partial cli args) options)]
     (cond
